42b8f495783332142164e0943bfa532207de7758
Victor Aprea <victor.aprea@wickeddevice.com>
2016-02-09 16:21:40 -0500

    moved hue interactions into huewrapper module

---
+++ huewrapper.js
@@ -0,0 +1,106 @@
+var HueApi = require("node-hue-api").HueApi;
+var hue = require("node-hue-api");
+var Promise = require("bluebird");
+var fs = require("fs");
+var path = require('path');
+Promise.promisifyAll(fs);
+
+module.exports = (function(bridgeip){
+    var api;
+    var bridegip;
+    var username = "temporary";
+    var credentials_filename = path.join(__dirname, 'huecredentials.json');
+    var ready = false;
+
+    Promise.try(function(){
+        // try and load the credentials file
+        return fs.readFileAsync(credentials_filename, "utf8");
+    }).then(function(fileContents){
+        // if it exists grab the 'username' out of it
+        var obj = JSON.parse(fileContents);
+        username = obj.username;
+    }).catch(function(exception){
+        // if it doesn't exist, no big deal, but we'll have to create a user
+        console.log("No config file created yet");
+    }).then(function(){
+        return hue.nupnpSearch().then(function(bridge) {
+            // find the hub
+            console.log("Hue Bridge Found: " + JSON.stringify(bridge));
+            bridegip = bridge[0].ipaddress;
+        }).then(function(){
+            // create the api object
+            api = new HueApi(bridegip, username);
+
+            // and ask for the config, to validate your authentication
+            return api.config();
+        }).then(function(config){
+            // check whether the existing username is authenticated
+            console.log("Hue Bridge API Version: " + config.apiversion);
+            if(!config.ipaddress){ // not authenticated
+                // if not, create a new user
+                return createNewUser();
+            }
+            else{
+                console.log("Successfully Logged in with Existing User");
+                ready = true;
+            }
+        });
+    });
+
+    function createNewUser(){
+        return api.registerUser(bridegip, "Air Quality Egg").then(function(result){
+            console.log("New User Successfully Created");
+            username = result;
+            ready = true;
+            return fs.writeFileAsync(credentials_filename, JSON.stringify({username: username}));
+        }).fail(function(err){
+            console.log(err);
+        }).done();
+    }
+
+    var getLights = function(){
+        if(ready){
+            var lights = [];
+            return Promise.try(function(){
+                return api.lights().then(function(results){
+                    if(results.lights){
+                        return results.lights;
+                    }
+                    return [];
+                });
+            }).map(function(light){
+                var obj = {};
+                obj[light.id] = {};
+                return obj;
+            }).map(function(task){
+                // for each light, ask the hub for its full state
+                var id = Object.keys(task)[0];
+                return Promise.try(function(){
+                    return api.lightStatus(id);
+                }).then(function(light){
+                    task[id] = light;
+                    return task;
+                });
+            }).filter(function(task){
+                // we only care about those lights which are reachable
+                var id = Object.keys(task)[0];
+                return task[id].state.reachable;
+            }).map(function(task){
+                var id = Object.keys(task)[0];
+                var obj = task[id];
+                obj.id = id;
+                return obj;
+            }).then(function(tasks){
+                    return tasks;
+            });
+        }
+        else{
+            return [];
+        }
+    };
+
+    return {
+        username: username,
+        getLights: getLights
+    };
+})();
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
-var express = require('express');
-var router = express.Router();
+var expressPromiseRouter = require("express-promise-router");
+var router = expressPromiseRouter();
+var Promise = require("bluebird");
+var huewrapper = require("../huewrapper");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/lights', function(req, res){
+  Promise.try(function(){
+    return huewrapper.getLights();
+  }).then(function(lights){
+    return res.json(lights);
+  });
 });
 
 module.exports = router;
